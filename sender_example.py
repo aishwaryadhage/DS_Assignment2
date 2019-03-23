d8ffcf6e61455f967af4b689dc2989ea2b927092
aishwaryadhage <aishwarya.dhage95@gmail.com>
2019-03-23 00:52:47 -0400

    Update sender_example.py

--- sender_example.py
+++ sender_example.py
@@ -1,4 +1,3 @@
-import zmq
 import sys
 import json
 import random
@@ -6,9 +5,21 @@ import socket
 import time
 import threading
 
-ports = [6001, 6002, 6003]
+ports = [6001, 6002]
 
+# Two processes sending message at the same time for peer to peer
 msg = {
+    "data": 'a',
+    "conn_type": "p2p",
+    "type": "app",
+    "timestamp": 0,
+    "ack_flag": 0,
+    "msg_id": 0,
+    "ports": [6002]
+}
+
+# Two processes sending message at the same time for peer to peer
+msg1 = {
     "data": 'a',
     "conn_type": "tom",
     "type": "app",
@@ -17,35 +28,24 @@ msg = {
     "msg_id": 0,
     "ports": [6002]
 }
-val = 0
 
 
 def sendmsg(port):
-    # global val
-    # val = val+1
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("127.0.0.1", port))
-    msg["data"] = port
+    msg["data"] = port % 10
     msg["msg_id"] = random.randint(1, 10000)
-    # time.sleep(3)
     s.send(json.dumps(msg))
-    # print "msg sent"
     s.close()
 
 
-# threading.Thread(
-#     target=sendmsg, args=(6001,)).start()
-
 lis = []
 count = 0
-for pid in range(3):
+for pid in range(2):
     lis.append(threading.Thread(
         target=sendmsg, args=(ports[pid],)))
-    # if count == 3:
-    #     break
-    # lis.append(threading.Thread(
-    #     target=sendmsg, args=(ports[pid],)))
-for i in range(3):
+
+for i in range(2):
     lis[i].start()
-for i in range(3):
+for i in range(2):
     lis[i].join()
